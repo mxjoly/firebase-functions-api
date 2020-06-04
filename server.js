226bdf03157a1a2fad4455b6db806401289c6a25
Maxime Joly <maxime.joly2808@gmail.com>
2020-06-04 14:35:14 +0200

    Add body parser to express

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const app = require('express')();
+const bodyParser = require('body-arser');
 
 const authRoute = require('./util/routes/auth');
 const userRoute = require('./util/routes/user');
+
+// Middlewares
+app.use(bodyParser.json());
 
 // Routes
 app.use('/auth', authRoute);
